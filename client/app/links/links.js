bdbb662ae9d8041a945c198b408343148c47cc5e
Kim Ha <kimberlygha@gmail.com>
2015-11-07 10:45:42 -0800

    Added filter search for links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,7 +3,7 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   $scope.data = {};
-  $scope.getLinks = function ( ) {
+  $scope.getLinks = function () {
     Links.getLink().then(function (links) {
       console.log('These are the links we getting', links);
       $scope.data = links.sort(function (a,b) {
